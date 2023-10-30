b39a0bc1bc0ddb0ce93e189cf65a60d9e7c4747e
Krishna Singh <officialkrishna009@gmail.com>
2023-10-30 21:39:00 +0545

    added

---
+++ GradeEncrypter.java
@@ -0,0 +1,11 @@
+
+
+public class GradeEncrypter {
+    public static void main(String[] args) {
+        char grade = 'b';
+        grade = (char)(grade + 8) ;
+        System.out.println(grade);
+        grade = (char)(grade - 8) ;
+        System.out.println(grade);
+    }
+}
---
+++ GreaterThanGivenNumber.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class GreaterThanGivenNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int num = sc.nextInt();
+        System.out.println(num>5);
+        sc.close();
+
+    }
+}
