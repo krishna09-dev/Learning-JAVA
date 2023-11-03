ffe00a0b9e515b83227b5d15b0f0f8258122d7df
Krishna Singh <officialkrishna009@gmail.com>
2023-11-03 16:20:01 +0545

    Update PrintArrayInReverse.java

--- PrintArrayInReverse.java
+++ PrintArrayInReverse.java
@@ -3,8 +3,8 @@
 public class PrintArrayInReverse {
     public static void main(String[] args) {
         int [] marks = {1,2,3,4,5,6,7,8,9,10};
-        int n = marks.length -1;
-        for (int i = n; i>0; i--) {
+        int n = marks.length-1 ;
+        for (int i = n; i>=0; i--) {
             System.out.println(marks[n]);
             n--;
         }
