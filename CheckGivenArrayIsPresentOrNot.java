49771cc23e8833ef75e874b747cf9b5ced130c50
Krishna Singh <officialkrishna009@gmail.com>
2023-12-13 13:07:36 +0545

    addeed commment

--- CheckGivenArrayIsPresentOrNot.java
+++ CheckGivenArrayIsPresentOrNot.java
@@ -1,14 +1,19 @@
+
+
 ///CheckGivenArrayIsPresentOrNot
 
 import java.util.Scanner;
 
 public class CheckGivenArrayIsPresentOrNot {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int [] num = {12 ,324 ,546 ,12 ,34 ,45 };
+        Scanner sc = new Scanner(System.in);
+        /// takes int as input to check 
         System.out.println("Enter a number to check: ");
         int toCheck = sc.nextInt();
+        // takes every elemets using for each
         for (int e : num) {
+            // take every element and check with give input
             if (e==toCheck){
                 System.out.println("its there");
                 break;
