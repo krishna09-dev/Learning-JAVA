214faaeafd97a81dbfecac84d9ed489df846b64d
Krishna Singh <officialkrishna009@gmail.com>
2023-12-13 13:07:17 +0545

    addeed commment

--- AverageOfAArray.java
+++ AverageOfAArray.java
@@ -5,9 +5,12 @@ public class AverageOfAArray {
     public static void main(String[] args) {
         int [] num = {12 ,324 ,546 ,656 ,34 ,45 };
         int sum = 0;
+        /// uses for each to get each elemet
         for (int element : num) {
+            ///adds each elemet to sum
             sum = sum+element;
         }
+        //finds the sum
         int average = sum/num.length;
         System.out.println(average);
     }
