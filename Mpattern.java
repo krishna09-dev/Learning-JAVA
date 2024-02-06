601fd28722a3e50af502bd4961c9fe4c334eeae2
krishna <officialkrishna009@gmail.com>
2024-02-06 17:48:31 +0545

    Update Mpattern.java

--- Mpattern.java
+++ Mpattern.java
@@ -4,20 +4,29 @@
 public class Mpattern {
 
     public static void main(String[] args) {
-        int count = 0, num = 3452;
+    //     int count = 0, num = 3452;
 
-    while (num != 0) {
-      num = num/10;
-      ++count;
-    }
+    // while (num != 0) {
+    //   num = num/10;
+    //   ++count;
+    // }
 
-    System.out.println("Number of digits: " + count);
-    int n =234;
-    int a=n;
-    for (int i = 1; i<=3; i++) {
-        a=a%10;
-        n=n-a;
-    System.out.println(n);
-    }
+    // System.out.println("Number of digits: " + count);
+    // int n =234;
+    // int a=n;
+    // for (int i = 1; i<=3; i++) {
+    //     a=a%10;
+    //     n=n-a;
+    // System.out.println(n);
+    // }
+
+//     int marks[][] = {{1,2,3},{4,5,6},{7,8,9}};
+//     for(int i=0; i<=marks.length;i++){
+// 	    for(int j=0; j<=marks.length;j++){
+//             System.out.println(i);
+//             System.out.println(j);
+// 		    // System.out.println(marks[i][j]);
+// 	}
+// }
     }
 }
